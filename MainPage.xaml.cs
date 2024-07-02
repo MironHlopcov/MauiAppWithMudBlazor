b1ca2e71896e7903411b97d697dc8b57a04208b8
miron <miron@DESKTOP-1A6VRJ4>
2024-07-02 18:18:56 +0300

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace MauiAppWithMudBlazor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BlazorWebViewHandler.BlazorWebViewMapper.AppendToMapping("CustomBlazorWebViewMapper", (handler, view) =>
+            {
+#if WINDOWS
+            //Workaround for WinUI splash screen
+            if (AppInfo.Current.RequestedTheme == AppTheme.Dark)
+            {
+                handler.PlatformView.DefaultBackgroundColor = Microsoft.UI.Colors.Black;
+            }
+#endif
+
+#if IOS || MACCATALYST
+                handler.PlatformView.ScrollView.Bounces = false;
+
+                handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+                handler.PlatformView.Opaque = false;
+#endif
+
+#if ANDROID
+                handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+
+                handler.PlatformView.OverScrollMode = Android.Views.OverScrollMode.Never;
+
+                handler.PlatformView.VerticalScrollBarEnabled = false;
+                handler.PlatformView.HorizontalScrollBarEnabled = false;
+
+                handler.PlatformView.ScrollbarFadingEnabled = true;
+
+               
+#endif
+            });
+
+            
+        }
+    }
+}
